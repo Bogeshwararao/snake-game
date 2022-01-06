a78821cff2e512e3ac6addec69f1d71af1dec2e2
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-01-06 20:37:31 +0530

    commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let direction = {x:0,y:0};
+const foodsound = new Audio('food.mp3.mp3');
+const gameoversound = new Audio('gameover.mp3');
+const movesound = new Audio('move.mp3');
+const musicsound = new Audio('music.mp3');
+let speed = 2;
+let lastpainttime = 0;
+
+
+
+function main(ctime){
+    window.requestAnimationFrame(main);
+    
+}
+
+
